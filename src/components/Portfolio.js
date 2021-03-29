6199cea96d8e4aa4f987aebcfdaa1546ebc1090f
Kristen Campbell <kristenc1031@gmail.com>
2021-03-29 17:11:13 -0600

    links

---
+++ src/components/Portfolio.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+export default class Porfolio extends Component {
+  render() {
+    let resumeData = this.props.resumeData;
+    return (
+      <section id="portfolio">
+      <div className="row">
+        <div className="twelve columns collapsed">
+          <h1>Check Out Some of My Projects.</h1>
+          <div id="portfolio-wrapper" className="bgrid-quarters">
+          {resumeData.portfolio && resumeData.portfolio.map((item)=>{
+              return(
+                <div className="columns portfolio-item">
+                  <div className="item-wrap">
+                    <a href="https://github.com/Jdouville8/DUFullStack-Project1">
+                      <img src={`${item.imgurl}`} className="item-img"/>
+                      <div className="overlay">
+                        <div onClick={() => { window.open (item.url) }} className="portfolio-item-meta">
+                          <h5>{item.name}</h5>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                </div>
+              )
+            })
+          }
+          </div>
+        </div>
+      </div>
+  </section>
+        );
+  }
+}
---
+++ src/resumeData.js
@@ -0,0 +1,89 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Kristen Campbell",
+    "role": "Full Stack Web Developer",
+    "linkedinId":"https://www.linkedin.com/in/kristen-campbell-7212b0196/",
+    "roleDescription": "I enjoy working on the Front End and am starting to work with iOS Development, using Swift and React Native.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/kristen-campbell-7212b0196/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/kristenc1031?tab=repositories",
+          "className":"fa fa-github"
+        },
+      ],
+    "aboutme":"I am currently finishing up my Full Stack Web Development class through University of Denver's Coding Boot Camp.",
+    "address":"Denver, CO",
+    "website":"https://github.com/kristenc1031",
+    "education":[
+      {
+        "UniversityName":"University of Denver's Coding Boot Camp",
+        "MonthOfPassing":"Graduate: April",
+        "YearOfPassing":"2021",
+        "Achievements":"HTML, CSS, JavaScript, Node.js, MongoDB, MYSQL, NoSQL, React.js"
+      },
+      {
+        "UniversityName":"Humphrey High School",
+        "MonthOfPassing":"Graduated: May",
+        "YearOfPassing":"2010",
+        
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Yuma Way LLC",
+        "specialization":"Human Resources Representative",
+        "MonthOfLeaving":"Aug 2020 - Nov 2020",
+        "Achievements": "Maintained personal information on Google Suite Drive and physical files, kept up to date  with compliance for 95+ employees. Designed and implemented recruitment planning processes intended to hire the quality  of candidates the company was searching for, ensured accuracy and compliance with  on-boarding documentation processes. Responded to incoming candidate resumes, conducted phone interviews, performed background checks, employment verification and reference checks. Made appropriate decisions for employee placement within the company including the final hiring decision. Responsible for: employee orientation and training, employee insurance benefits, set up employee on various platforms: BioTrack, Learn Brands, Metrc, When I Work, Responsible Vendor Training. Read MED Laws and Regulations to create a Standard Operating Procedure Manual for both Cultivation and Retail to ensure compliance within the company. Project management, made sure all policies listed in the SOP were read and understood by employees. Managed HR review sheet, created a new tracking system for HR reviews. Handled unemployment forms and child support paperwork, followed all HIPPA rules."
+      },
+      {
+        "CompanyName":"CC Brands, LLC",
+        "specialization":"Office/Sales Assistant",
+        "MonthOfLeaving":"May 2017 - March 2020",
+        "Achievements":"Assisted in navigating sales planning in highly regulated industry. Assisted the territory representatives with order fulfillment, print and emailed client  invoices using QuickBooks Desktop. Entered orders into METRC, a U.S. Government and State Tracking Data System. Managed client database while providing recurring reports and territory summaries. Responsible for all incoming payments for 500+ accounts, created and maintained client filing system."
+      }
+    ],
+    
+    "portfolio":[
+      {
+        "name":"Music Artist Search",
+        "description":"mobileapp",
+        "url": "https://github.com/Jdouville8/DUFullStack-Project1",
+        "imgurl":"https://images.unsplash.com/photo-1572177812156-58036aae439c?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NHx8cHJvamVjdHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+      },
+      {
+        "name":"Note Taker",
+        "description":"mobileapp",
+        "url": "https://hidden-ravine-39485.herokuapp.com/",
+        "imgurl":"https://images.unsplash.com/photo-1572177812156-58036aae439c?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NHx8cHJvamVjdHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+      },
+      {
+        "name":"Workout Tracker",
+        "description":"mobileapp",  
+        "url": "https://fierce-gorge-75966.herokuapp.com/",
+        "imgurl":"https://images.unsplash.com/photo-1572177812156-58036aae439c?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NHx8cHJvamVjdHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+      },
+      {
+        "name":"Daily Horoscope App",
+        "description":"mobileapp",
+        "url": "https://damp-mountain-65863.herokuapp.com/",
+        "imgurl":"https://images.unsplash.com/photo-1572177812156-58036aae439c?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NHx8cHJvamVjdHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+      }
+    ],
+    // "testimonials":[
+    //   {
+    //     "description":"This is a sample testimonial",
+    //     "name":"Some technical guy"
+    //   },
+    //   {
+    //     "description":"This is a sample testimonial",
+    //     "name":"Some technical guy"
+    //   }
+    // ]
+  }
+  
+  export default resumeData
