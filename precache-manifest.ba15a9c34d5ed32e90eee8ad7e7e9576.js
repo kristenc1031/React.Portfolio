47d15d7ced474d36124935442f90e8e45caa536d
Kristen Campbell <kristenc1031@gmail.com>
2021-03-30 16:23:58 -0600

    Updates

---
+++ precache-manifest.ba15a9c34d5ed32e90eee8ad7e7e9576.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3da7c4fa8047da59d5e8",
+    "url": "/React.Portfolio/static/css/main.34a0dd9b.chunk.css"
+  },
+  {
+    "revision": "3da7c4fa8047da59d5e8",
+    "url": "/React.Portfolio/static/js/main.3da7c4fa.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/React.Portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "9788263daa3dc239560a",
+    "url": "/React.Portfolio/static/js/runtime~main.9788263d.js"
+  },
+  {
+    "revision": "b30c458e8167a0d75f59a1205d3a2d60",
+    "url": "/React.Portfolio/index.html"
+  }
+];
